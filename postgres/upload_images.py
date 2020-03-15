3c776c79d7fcd0a3d4842848d1fb6ba3f579905e
erebuseternal <marcelsanders96@gmail.com>
2020-03-15 02:49:00 +0000

    image uploader uses server

--- postgres/upload_images.py
+++ postgres/upload_images.py
@@ -2,52 +2,62 @@ import argparse
 import psycopg2
 import json
 import os
+import requests
 
 
-class LocalBroker(object):
-    def __init__(self, directory='/root/local_storage'):
-        self.directory = directory
-        if not os.path.exists(self.directory):
-            os.mkdir(self.directory)
-            
-    def upload(self, data, identifier, case):
-        directory = '/'.join([self.directory, case])
-        if not os.path.exists(directory):
-            os.mkdir(directory)
-        with open('/'.join([directory, str(identifier) + '.jpg']), 'wb') as fh:
-            fh.write(data)
+class DockerBroker(object):
+    def __init__(self, hostname, port):
+        self.base_url = 'http://%s:%s' % (hostname, port)
+        
+    def upload(self, fh, bucket, key):
+        url = '%s/push?bucket=%s&key=%s.jpg' % (self.base_url, bucket, key)
+        files = {'upload': fh}
+        response = requests.post(url, files=files)
+        if not str(response.status_code).startswith('2'):
+            raise Exception('upload failed (%s): %s' % (url, response.status_code))
+    
+    def download(self, bucket, key):
+        url = '%s/pull?bucket=%s&key=%s.jpg' % (self.base_url, bucket, key)
+        response = requests.get(url)
+        if not str(response.status_code).startswith('2'):
+            raise Exception('download failed (%s): %s' % (url, response.status_code))
+        return response.content
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--meta', help='path to meta data',
                         type=str)
-    parser.add_argument('--host', help='hostname of postgres db',
+    parser.add_argument('--sql', help='hostname of postgres db',
                         type=str)
     parser.add_argument('--password', help='password for postgres db',
+                        type=str)
+    parser.add_argument('--store', help='hostname of storage',
                         type=str)
     args = parser.parse_args()
     if args.meta is None:
         parser.error('please specify --meta')
-    if args.host is None:
-        parser.error('please specify --host')
+    if args.sql is None:
+        parser.error('please specify --sql')
     if args.password is None:
         parser.error('please specify --password')
+    if args.store is None:
+        parser.error('please specify --store')
         
     # create connection to db
     conn = psycopg2.connect('host=%s dbname=postgres user=postgres password=%s' %
-                            (args.host, args.password))
+                            (args.sql, args.password))
     with open(args.meta, 'r') as fh:
         meta_data = json.load(fh)
     cursor = conn.cursor()
-    broker = LocalBroker()
+    broker = DockerBroker(args.store, 5000)
     records = []
     for data in meta_data:
         upload_id = data['upload_id']
         image_path = data['image_path']
         observation_time = data['observation_time']
         with open(image_path, 'rb') as fh:
-            broker.upload(fh.read(), upload_id, 'upload')
+            broker.upload(fh, 'upload', upload_id)
         records.append((upload_id, observation_time))
     cursor.executemany(
         "INSERT INTO uploads VALUES (%s, %s)",
