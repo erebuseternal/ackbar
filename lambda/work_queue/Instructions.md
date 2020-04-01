ff638d9c714502a7f6495617814b537b59da14f3
erebuseternal <marcelsanders96@gmail.com>
2020-04-01 13:33:47 +0000

    work queue populator job draft

---
+++ lambda/work_queue/Instructions.md
@@ -0,0 +1,9 @@
+# ackbar
+
+## Docker Instructions
+
+#### Building the Job
+`docker build -t "mgietzmann/populate_work_queue:latest" .`
+
+#### Running the Job
+`docker run mgietzmann/populate_work_queue:latest`
---
+++ lambda/work_queue/populate_work_queue.py
@@ -0,0 +1,73 @@
+import redis
+from datetime import datetime
+import psycopg2
+
+
+if __name__ == '__main__':
+    redis_client = redis.Redis(
+        host=os.environ['REDIS_HOSTNAME'],
+        port=6379,
+        password=os.environ['REDIS_PASSWORD']
+    )
+    
+    conn = psycopg2.connect('host=%s dbname=postgres user=postgres password=%s' 
+                            % (os.environ['POSTGRESS_HOSTNAME'], 
+                               os.environ['POSTGRES_PASSWORD']))
+    
+    # first we need to grab a list of all of all of the projects
+    cursor = conn.cursor()
+    sql = '''
+    SELECT DISTINCT(project) FROM classifications
+    '''
+    cursor.execute(sql)
+    projects = [result[0] for result in cursor]
+    print('Found projects ', projects)
+    
+    # next we can update the streams for each project
+    time_format = '%Y-%m-%d %H:%M:%S.%f'
+    field_to_index = {
+        'project': 0,
+        'upload_id': 1,
+        'observation_time': 2,
+        'detection_id': 3,
+        'class_name': 4,
+        'c0': 6, 'c1': 7,
+        'c2': 8, 'c3': 9,
+        'c4': 10
+    }
+    for project in projects:
+        print('Working on project ', project)
+        stream_name = '%s_validation_work_stream' % project
+        # first we check to see if the stream has anything in it
+        if redis_client.xlen(stream_name) == 0:
+            latest_time = datetime.datetime(1970, 1, 1)
+            print('Stream currently empty')
+        else:
+            # if the stream does exist we grab the latest entry
+            latest_entry = redis_client.xrevrange(stream_name, count=1)[0][1]
+            latest_time = \
+                datetime.strptime(latest_entry[b'observation_time'].decode("utf-8"),
+                                  time_format)
+            print('Latest time ', latest_time)
+        # next we query all the validations that come after this point in
+        # time
+        sql = '''
+        SELECT * FROM classifications
+        WHERE NOT validated AND project = '%s'
+        AND observation_time > '%s'
+        ORDER BY observation_time ASC
+        ''' % (project, latest_time)
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        records = []
+        for result in cursor:
+            record = {
+                k: result[v] for k, v in field_to_index.items()
+            }
+            record['observation_time'] = \
+                record['observation_time'].strftime(time_format)
+            records.append(record)
+        # now we can add to the stream
+        for record in records:
+            redis_client.xadd(stream_name, record)
+        print('Added ', len(records), ' to stream')
