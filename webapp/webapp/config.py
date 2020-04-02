897de53873bd19f1c24d58da0882a5efcb522b4e
erebuseternal <marcelsanders96@gmail.com>
2020-04-02 14:00:11 +0000

    saving progress (everything is broken so you'll want to go back a commit)

--- webapp/webapp/config.py
+++ webapp/webapp/config.py
@@ -2,7 +2,8 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-
+    REDIS_PASSWORD = 'ackbar'
+    REDIS_HOST = 'localhost'
     @staticmethod
     def init_app(app):
         pass
